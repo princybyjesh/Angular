740a5ff6f0d067ebd22c9780ed5b8c545c934963
unknown <princypk12345@gmail.com>
2022-09-30 11:14:49 +0530

    two way binding

--- bankapp2/src/app/login/login.component.ts
+++ bankapp2/src/app/login/login.component.ts
@@ -8,21 +8,52 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 aim="your perfect banking partner"
 acnt="enter your acnumber"
-acno=""
-psw=""
+acno=''
+psw=''
   UserDetails:any={
     1000:{acno:1000,username:"amal",password:123,balance:100000},
     1001:{acno:1000,username:"anu",password:122,balance:100000},
     1002:{acno:1000,username:"amaal",password:133,balance:100000},
     1003:{acno:1000,username:"ammu",password:113,balance:100000},
   }
-  constructor() { }
+  
+  constructor() { 
+
+    
+    
+  }
 
   ngOnInit(): void {
   }
-// login(){
-//   var acnum=this.acno
-//   var psw=this.psw
+  
+login(){
+  var acnum=this.acno
+  var psww=this.psw
+
+  console.log(acnum);
+  console.log(psww);
+  let userDetails=this.UserDetails
+  if(acnum in userDetails ){
+    console.log(userDetails[acnum]['password']);
+    if(psww==userDetails[acnum]['password']){
+      alert("login succcess")
+    }
+    else{
+      alert("password incorrect")
+    }
+  }
+  else{
+    alert("user not exist")
+  }
+}
+}
+// login(a:any,b:any){
+//   console.log(a.value);
+//   console.log(b.value);
+  
+  
+//   var acnum=a.value
+//   var psw=b.value
 //   let userDetails=this.UserDetails
 //   if(acnum in userDetails ){
 //     if(psw==userDetails[acnum]['password']){
@@ -36,34 +67,14 @@ psw=""
 //     alert("user not exist")
 //   }
 // }
-login(a:any,b:any){
-  console.log(a.value);
-  console.log(b.value);
-  
-  
-  var acnum=a.value
-  var psw=b.value
-  let userDetails=this.UserDetails
-  if(acnum in userDetails ){
-    if(psw==userDetails[acnum]['password']){
-      alert("login succcess")
-    }
-    else{
-      alert("password incorrect")
-    }
-  }
-  else{
-    alert("user not exist")
-  }
-}
-accnoChange(event:any){
-this.acno=event.target.value
-console.log(this.acno)
-}
-pswChange(event:any){
-this.psw=event.target.value
-console.log(this.psw);
+// accnoChange(event:any){
+// this.acno=event.target.value
+// console.log(this.acno)
+// }
+// pswChange(event:any){
+// this.psw=event.target.value
+// console.log(this.psw);
 
-}
-}
+// }
+// }
 
