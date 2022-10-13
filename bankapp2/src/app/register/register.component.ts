fd04fb75e6aec960e5466eb0c2f375b92310a80c
unknown <princypk12345@gmail.com>
2022-10-13 17:23:43 +0530

    first validator

--- bankapp2/src/app/register/register.component.ts
+++ bankapp2/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -11,15 +12,19 @@ export class RegisterComponent implements OnInit {
 uname=""
 acno=""
 psw=""
-  constructor(private ds:DataService,private router:Router) { }
+// model to registration form
+registerForm=this.fb.group({
+  uname:['',[Validators.required,Validators.pattern('[a-zA-Z+]')]],acno:['',[]],pswd:['',[]]
+})
+  constructor(private fb:FormBuilder, private ds:DataService,private router:Router) { }
 
   ngOnInit(): void {
   }
 register(): void{
   alert("registerd")
-  var uname=this.uname
-  var acno=this.acno
-  var psw=this.psw
+  var uname=this.registerForm.value.uname
+  var acno=this.registerForm.value.acno
+  var psw=this.registerForm.value.pswd
   const result= this.ds.register(acno,uname,psw)
 if(result){
   alert("registered")
